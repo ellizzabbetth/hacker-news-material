1f4286135653168ce560e32a4171e540b64f2240
elizabeth <ellizzabbetth@gmail.com>
2020-07-11 17:27:12 -0400

    first

---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using hacker_news_nextech;
+namespace test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+Assert.True(true);
+        }
+    }
+}
